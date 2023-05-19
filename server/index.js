6cd4614a32e5657bfa37c39d86eabce17af96825
brnt-toast <yoyo.beerme@gmail.com>
2023-05-19 04:37:09 -0400

    added: morgan

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express()
 const port = 3999
 
+app.use(morgan('combined'))
+
 app.get('/', (req, res) => {
-    res.send("Hello World")
+    res.send("Hello World\n")
 })
 
 app.listen(port, () => {
