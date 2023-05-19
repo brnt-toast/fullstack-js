503b05651348ad8c94e7c4526088bfd5d0456232
brnt-toast <yoyo.beerme@gmail.com>
2023-05-19 05:02:58 -0400

    install: cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors"
 
 const app = express()
 const port = 3999
 
 app.use(morgan('combined'))
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send("Hello World\n")
